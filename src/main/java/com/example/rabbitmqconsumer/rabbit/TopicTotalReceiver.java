60c2b9956ad48a7688088f81ca46e7b1e2bed1ff
wcybaonier <baonierlove@163.com>
2021-04-07 09:05:41 +0800

    rabbitmq消费者第一次提交

---
+++ src/main/java/com/example/rabbitmqconsumer/rabbit/TopicTotalReceiver.java
@@ -0,0 +1,16 @@
+package com.example.rabbitmqconsumer.rabbit;
+ 
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import java.util.Map;
+
+@Component
+@RabbitListener(queues = "topic.woman")
+public class TopicTotalReceiver {
+ 
+    @RabbitHandler
+    public void process(Map testMessage) {
+        System.out.println("TopicTotalReceiver消费者收到消息  : " + testMessage.toString());
+    }
+}
